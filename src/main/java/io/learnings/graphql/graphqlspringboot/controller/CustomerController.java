73e49bc43d2df2d4c312ce146b2e4e80acf5c72e
Ajit Shukla <official.aj.shukla@gmail.com>
2025-03-26 11:08:45 +0530

    Initial commit

--- src/main/java/io/learnings/graphql/graphqlspringboot/controller/CustomerController.java
+++ src/main/java/io/learnings/graphql/graphqlspringboot/controller/CustomerController.java
@@ -1,4 +1,34 @@
 package io.learnings.graphql.graphqlspringboot.controller;
 
+import io.learnings.graphql.graphqlspringboot.repository.entities.Customer;
+import io.learnings.graphql.graphqlspringboot.repository.entities.CustomerRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
 public class CustomerController {
+
+    private CustomerRepository repository;
+
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @QueryMapping("customers") //method name or name in this annotation should match schema.graphqls.query.customers
+    public List<Customer> getCustomers() {
+        return repository.findAll();
+    }
+
+    @QueryMapping("customerById")
+    public Customer getCustomerById(@Argument Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @QueryMapping("customerById")
+    public Customer customerByEmail(@Argument String email) {
+        return repository.findByEmail(email);
+    }
 }
--- src/main/java/io/learnings/graphql/graphqlspringboot/repository/entities/CustomerRepository.java
+++ src/main/java/io/learnings/graphql/graphqlspringboot/repository/entities/CustomerRepository.java
@@ -1,4 +1,8 @@
 package io.learnings.graphql.graphqlspringboot.repository.entities;
 
-public class CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String email);
 }
